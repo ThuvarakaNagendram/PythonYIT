c80dbe962d6bb7e446c11ede68c8f9f71e21e5e3
ThuvarakaNagendram <111124391+ThuvarakaNagendram@users.noreply.github.com>
2026-02-03 14:39:08 +0530

    Add python files

---
+++ Excercise.py
@@ -0,0 +1,18 @@
+id = 30001;
+name = "Seelan";
+gradeName = "10A";
+nicNo = "876780116V";
+
+#method-1
+print("********************");
+print("Student Information");
+print("--------------------");
+print("My Id is "+str(id));
+print("My name is "+name);
+print("My grade is "+gradeName);
+print("My NIC no is "+nicNo);
+print("--------------------");
+
+#method-2
+print("");
+
---
+++ first.py
@@ -0,0 +1,21 @@
+id = 1;
+name = "Pommu";
+age = 10;
+
+"""
+print ("My ID is "+ str(id))
+print ("My name is "+ name)
+print ("My age is "+ str(age))
+"""
+print("My Id is "+str(id),"\nMy name is "+name,"\nMy age is "+str(age))
+
+"""
+print(type (id));
+print(type(name));
+"""
+
+#output = f"My id is {id} \nMy name is {name} \nMy age is {age}";
+""""output = "My id is {0} \nMy name is {1} \nMy age is {2}".format(id,name,age);
+print(output);
+"""
+#print("My Id is %d \nMy Name is %s \nMy Age is %d"%(id,name,age));
